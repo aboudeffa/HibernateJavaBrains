1d41159f6631a7591cbf4694a1e5dad9db578377
BOUDEFFA AMIN <aboudefa@gmail.com>
2016-11-23 22:01:10 +0100

    retrieving Objects using session

--- src/org/javabrains/koushik/dto/UserDetails.java
+++ src/org/javabrains/koushik/dto/UserDetails.java
@@ -1,29 +1,59 @@
 package org.javabrains.koushik.dto;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
-@Entity (name="USER_DETAILS")
+@Entity
+@Table (name="USER_DETAILS")
 public class UserDetails {
-	
-	private int userId;
-	private String userName;
-	
 	@Id
-	@Column (name="USER_ID")
+	private int userId;
+	//@Transient : to ignore this field
+	private String userName;
+	//@Temporal(TemporalType.DATE) : display only the date
+	//@Temporal (TemporalType.TIME) : display only the time
+	private Date joinedDate;
+	@Lob
+	private String address;
+	private String description;
+
 	public int getUserId() {
 		return userId;
 	}
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
-	@Column (name="USER_NAME")
 	public String getUserName() {
-		return userName +" from getter";
+		return userName;
 	}
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+	public Date getJoinedDate() {
+		return joinedDate;
+	}
+	public void setJoinedDate(Date joinedDate) {
+		this.joinedDate = joinedDate;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
 	}
 	
 }
--- src/org/koushik/hibernate/HibernateTest.java
+++ src/org/koushik/hibernate/HibernateTest.java
@@ -1,5 +1,7 @@
 package org.koushik.hibernate;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -11,12 +13,24 @@ public class HibernateTest {
 		UserDetails user = new UserDetails();
 		user.setUserId(1);
 		user.setUserName("First User");
+		user.setJoinedDate(new Date());
+		user.setAddress("First user's address");
+		user.setDescription("Description of the first user goes here");
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(user);
 		session.getTransaction().commit();
+		session.close();
+		
+		user = null;
+		
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		user = session.get(UserDetails.class, 1);
+		System.out.println("User name retrieved is "+user.getUserName());
+		
 		
 	}
 

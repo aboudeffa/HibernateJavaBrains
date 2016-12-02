fe31856c7a0d29ec89927d42baec92175908d3ac
BOUDEFFA AMIN <aboudefa@gmail.com>
2016-12-02 22:03:36 +0100

    incrementing primary key

--- src/org/javabrains/koushik/dto/UserDetails.java
+++ src/org/javabrains/koushik/dto/UserDetails.java
@@ -1,30 +1,17 @@
 package org.javabrains.koushik.dto;
 
-import java.util.Date;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.Transient;
 
 @Entity
 @Table (name="USER_DETAILS")
 public class UserDetails {
-	@Id
+	
+	@Id @GeneratedValue
 	private int userId;
-	//@Transient : to ignore this field
 	private String userName;
-	//@Temporal(TemporalType.DATE) : display only the date
-	//@Temporal (TemporalType.TIME) : display only the time
-	private Date joinedDate;
-	@Lob
-	private String address;
-	private String description;
-
 	public int getUserId() {
 		return userId;
 	}
@@ -37,23 +24,4 @@ public class UserDetails {
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	public Date getJoinedDate() {
-		return joinedDate;
-	}
-	public void setJoinedDate(Date joinedDate) {
-		this.joinedDate = joinedDate;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
 }
--- src/org/koushik/hibernate/HibernateTest.java
+++ src/org/koushik/hibernate/HibernateTest.java
@@ -1,7 +1,5 @@
 package org.koushik.hibernate;
 
-import java.util.Date;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -10,27 +8,22 @@ import org.javabrains.koushik.dto.UserDetails;
 public class HibernateTest {
 
 	public static void main(String[] args) {
+		
 		UserDetails user = new UserDetails();
-		user.setUserId(1);
+//		user.setUserId(1);
 		user.setUserName("First User");
-		user.setJoinedDate(new Date());
-		user.setAddress("First user's address");
-		user.setDescription("Description of the first user goes here");
+		
+		UserDetails user2 = new UserDetails();
+//		user2.setUserId(2);
+		user2.setUserName("Second User");
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(user);
+		session.save(user2);
 		session.getTransaction().commit();
 		session.close();
-		
-		user = null;
-		
-		session = sessionFactory.openSession();
-		session.beginTransaction();
-		user = session.get(UserDetails.class, 1);
-		System.out.println("User name retrieved is "+user.getUserName());
-		
 		
 	}
 
